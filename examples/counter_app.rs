58e309b24fbe3c8a6e56699cf7d7cfde5ae0f2d7
David Bryson <dbryson@mitre.org>
2018-08-22 13:58:54 -0500

    added counter example

---
+++ examples/counter_app.rs
@@ -0,0 +1,71 @@
+extern crate abci;
+extern crate byteorder;
+
+use byteorder::{BigEndian, ByteOrder};
+
+// Simple counter application.  Its only state is a u64 count
+// We use BigEndian to serialize the data across transactions calls
+struct CounterApp {
+    count: u64,
+}
+
+impl CounterApp {
+    fn new() -> CounterApp {
+        CounterApp { count: 0 }
+    }
+}
+
+fn convert_tx(tx: &[u8]) -> u64 {
+    if tx.len() < 8 {
+        let pad = 8 - tx.len();
+        let mut x = vec![0; pad];
+        x.extend_from_slice(tx);
+        return BigEndian::read_u64(x.as_slice());
+    }
+    BigEndian::read_u64(tx)
+}
+
+impl abci::Application for CounterApp {
+    // Validate transactions.  Rule:  count transactions must be incremental
+    fn check_tx(&mut self, req: &abci::RequestCheckTx) -> abci::ResponseCheckTx {
+        // Get the Tx [u8] and convert to u64
+        let c = convert_tx(req.get_tx());
+        let mut resp = abci::ResponseCheckTx::new();
+
+        // Validation logic
+        if c != self.count + 1 {
+            resp.set_code(1);
+            resp.set_log(String::from("Count must be incremental!"));
+            return resp;
+        }
+
+        // Update state to keep state correct for next check_tx call
+        self.count = c;
+        resp
+    }
+
+    fn deliver_tx(&mut self, req: &abci::RequestDeliverTx) -> abci::ResponseDeliverTx {
+        // Get the Tx [u8]
+        let c = convert_tx(req.get_tx());
+        // Update state
+        self.count = c;
+        // Return default code 0 == bueno
+        abci::ResponseDeliverTx::new()
+    }
+
+    fn commit(&mut self, _req: &abci::RequestCommit) -> abci::ResponseCommit {
+        // Create the response
+        let mut resp = abci::ResponseCommit::new();
+        // Convert count to bits
+        let mut buf = [0; 8];
+        BigEndian::write_u64(&mut buf, self.count);
+        // Set data so last state is included in the block
+        resp.set_data(buf.to_vec());
+        resp
+    }
+}
+
+fn main() {
+    let addr = "127.0.0.1:26658".parse().unwrap();
+    abci::run(addr, CounterApp::new());
+}
